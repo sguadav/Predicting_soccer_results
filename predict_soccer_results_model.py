a8bddb57d13a5eb13908f9acaf00e34d393cc11b
Santiago Guada <sguada@purdue.edu>
2020-07-03 19:01:09 -0500

    Final commit, minor changes

--- predict_soccer_results_model.py
+++ predict_soccer_results_model.py
@@ -12,15 +12,18 @@ from sklearn.metrics import mean_squared_error, r2_score
 df = pd.read_csv('eda_data/Premier_eda_data.csv')
 
 # Get dummy columns
-df_dum = pd.get_dummies(df)
+df_dum_home = pd.get_dummies(df)
+df_dum_away = pd.get_dummies(df)
 
 # Train and Testing data
-X_home = df_dum.drop('Results_1', axis=1)
-y_home = df_dum.Results_1.values
+df_dum_home = df_dum_home.drop('Results_2', axis=1)             # Drop the result from the rival in the match
+X_home = df_dum_home.drop('Results_1', axis=1)
+y_home = df_dum_home.Results_1.values
 X_home_train, X_home_test, y_home_train, y_home_test = train_test_split(X_home, y_home, test_size=0.2, random_state=42)
 
-X_away = df_dum.drop('Results_2', axis=1)
-y_away = df_dum.Results_2.values
+df_dum_away = df_dum_away.drop('Results_1', axis=1)             # Drop the result from the rival in the match
+X_away = df_dum_away.drop('Results_2', axis=1)
+y_away = df_dum_away.Results_2.values
 X_away_train, X_away_test, y_away_train, y_away_test = train_test_split(X_away, y_away, test_size=0.2, random_state=42)
 
 # # Multiple Linear Regression
